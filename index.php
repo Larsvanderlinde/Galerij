8802f0a4e3cdb748547a6cca7ab1a55b9b4343e3
Lars <97087764@st.deltion.nl>
2022-01-18 12:38:46 +0100

    Mijn derde dingentje

--- index.php
+++ index.php
@@ -1,48 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   
+<link rel="stylesheet"  href="inloggen.css">
+
     <title>Document</title>
 </head>
 <body>
     <div> 
-        <h3>Login scherm zie je toch </h3>
-
-        <form method = "POST"> 
-            <table>
-                 <tr>
-                     <td> Gebruikersnaam </td> 
-                     <td> <input type = "text" name = "gebruikersnaam" >  </td>
-                </tr>
-                <tr>
-                 <td> Wachtwoord    </td> 
-                 <td> <input type = "text" name = "wachtwoord"> </td>
-                 <tr>
-                     <td></td>
-                     <td> <input type = "submit" value = "login"> </td>
-                </tr>
-            </table>
-        </form>
-
-    </div>
-
-    <?php
-        if ($_POST) {
-            $gb=$_POST['gebruikersnaam'];
-            $ww=$_POST['wachtwoord'];
-
-            if ( $gb=='LarsMan1222' && $ww=='lars' ) {
-                header("location:gallery.php");
-                exit ();
-            }
-
-            else {
-                echo "Verkeerde gebruikersnaam/wachtwoord";
-            }
-        }
         
 
-    ?>
+      <form action = "gallery.php" method= "post" > 
+      <h2>Inloggen </h2>
+      <label> Gebruikersnaam </label>
+      <input type = "text" name = "gnaam" placeholder = "Gebruikersnaam"> <br>
+      <label> Wachtwoord </label>
+      <input type = "text" name = "wachtwoord" placeholder = "Wachtwoord"> <br>
+      <button type = "submit"> Inloggen </button>
+      </form>
 
     
 </body>
